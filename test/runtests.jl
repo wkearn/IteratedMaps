3719e0064bd62cd1c8008383478dbde0764a2a1a
Will Kearney <wkearn@gmail.com>
2015-07-30 13:56:01 -0400

    Add basic test coverage

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,25 @@
 using IteratedMaps
 using Base.Test
 
-# write your own tests here
-@test 1 == 1
+
+# A logistic map with parameter 2 converges to 0.5
+lm = LogisticMap(2)
+
+for i in take(lm(0.5),100)
+    @test i == 0.5
+end
+
+# Test the alternative syntax for map generation
+for i in 1:100
+    @test collect(take(lm(0.2),100))[i] == collect(take(LogisticMap(2,0.2),100))[i]
+end
+
+# Check the Henon map calculates correctly
+@test collect(take(HenonMap([1.4,0.3],[0.2,0.3]),2)) == {[0.2,0.3],[1.244,0.06]}
+
+# Same with the Duffing map
+@test collect(take(DuffingMap([2.75,0.2],[1.0,0.0]),2)) == {[1.0,0.0],[0.0,-0.2]}
+
+# And the Tent map
+
+@test collect(take(TentMap(2,0.2),2)) = [0.2,0.4]
